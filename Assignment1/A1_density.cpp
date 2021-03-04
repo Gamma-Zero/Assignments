c12db07276521850ebedf51a36bc2e400c1f5611
Gamma-Zero <cs1190380@iitd.ac.in>
2021-03-04 12:57:49 +0530

    Changes made

--- Assignment1/A1_density.cpp
+++ Assignment1/A1_density.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
     VideoCapture cap("vid.mp4");
+    Mat img = imread("C:\\Users\\PARTH\\Desktop\\cop290 assignments\\Assignment1\\empty.jpg");
     if (!cap.isOpened())
     {
         cout << "Error, cannot open the video.";
@@ -21,10 +22,10 @@ int main()
     namedWindow(name1, WINDOW_NORMAL);
     Ptr<BackgroundSubtractor> bgs= createBackgroundSubtractorMOG2();
     Mat frame,subt;
-    int frame_width = cap.get(CAP_PROP_FRAME_WIDTH); 
-    int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
-    Size frame_size(frame_width, frame_height);
-    VideoWriter wr("Small.mp4", VideoWriter::fourcc('M', 'J', 'P', 'G'), 5, frame_size, true);
+    //int frame_width = cap.get(CAP_PROP_FRAME_WIDTH); 
+    //int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
+    //Size frame_size(frame_width, frame_height);
+    //VideoWriter wr("Small.mp4", VideoWriter::fourcc('M', 'P', '4', 'V'), 5, frame_size, true);
     while (true)
     {
         bool next = cap.read(frame);
@@ -35,8 +36,8 @@ int main()
             break;
         next = cap.read(frame);
         if (!next) break;
-        bgs->apply(frame, subt);
-        wr.write(subt);
+        bgs->apply(frame, subt, 0);
+        //wr.write(subt);
         imshow(name, frame);
         imshow(name1, subt);
         int press = waitKey(10);
@@ -47,17 +48,7 @@ int main()
         }
 
     }
-    wr.release();
+    cap.release();
+    //wr.release();
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
