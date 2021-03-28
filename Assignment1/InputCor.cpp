381d8b89108c5c05bde77720f27f8d28bb8ad066
Gamma-Zero <78369771+Gamma-Zero@users.noreply.github.com>
2021-03-28 23:39:15 +0530

    Takes input coordinates of user

---
+++ Assignment1/InputCor.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+vector<Point2f>cor_init;
+void Click(int event, int x, int y, int flags, void* userdata)
+{
+    if (event == EVENT_LBUTTONDOWN)
+    {
+        cor_init.push_back(Point2f(x, y));
+    }
+}
+int main()
+{
+    ofstream file2;
+    file2.open("cor.csv");
+    Mat img;
+    VideoCapture cap2("vid.mp4");
+    cap2 >> img;
+    cvtColor(img, img, COLOR_BGR2GRAY);
+    namedWindow("Display", WINDOW_NORMAL);
+    setMouseCallback("Display", Click, NULL);
+    imshow("Display", img);
+    waitKey(0);
+    for (int i = 0; i < cor_init.size(); ++i)
+        file2 << cor_init[i].x << "," << cor_init[i].y << "\n";
+    file2.close();
+
+}
