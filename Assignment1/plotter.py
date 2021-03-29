7e1ed82a037a0592a5147e7c3f1f27132b32f418
ananjan-nandi-9 <tgk.ananjan@gmail.com>
2021-03-29 21:50:55 +0530

    Corrected Sparseflow and changed plotter

--- Assignment1/plotter.py
+++ Assignment1/plotter.py
@@ -1,14 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data=np.genfromtxt("moving.csv",delimiter=",",names=["x","y"])
-plt.plot(data['x'],data['y'],label='Moving Density')
-plt.xlabel('Time')
-plt.ylabel('Density')
-
-
-data=np.genfromtxt("stationary.csv",delimiter=",",names=["x","y"])
+data=np.genfromtxt("dynamic.csv",delimiter=",",names=["x","y"])
 plt.plot(data['x'],data['y'], label='Queue Density')
 plt.legend()
-plt.savefig("stationary.png")
+plt.savefig("dynamic.png")
 
