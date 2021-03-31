0d393abb10795bac2c7681863ea837d6ec71c223
ananjan-nandi-9 <tgk.ananjan@gmail.com>
2021-03-31 13:24:07 +0530

    Data updated

--- Assignment1/DenseFlow.cpp
+++ Assignment1/DenseFlow.cpp
@@ -6,7 +6,6 @@ typedef std::chrono::high_resolution_clock Clock;
 using namespace cv;
 using namespace std;
 vector<Point2f> cor_init, cor_fin;
-double error1 = 0;
 vector<double> baseline;
 ofstream file;
 ofstream file1;
@@ -18,18 +17,8 @@ void Click(int event, int x, int y, int flags, void *userdata)
 	}
 }
 
-void Sparse(string pq)
+void Dense(string pq)
 {
-	vector<Scalar> colors;
-	RNG rng;
-	for (int i = 0; i < 100; i++)
-	{
-		int r = rng.uniform(0, 256);
-		int g = rng.uniform(0, 256);
-		int b = rng.uniform(0, 256);
-		colors.push_back(Scalar(r, g, b));
-	}
-
 	VideoCapture cap2(pq);
 	if (!cap2.isOpened())
 	{
@@ -55,19 +44,15 @@ void Sparse(string pq)
 	cvtColor(empty, empty, COLOR_BGR2GRAY);
 	warpPerspective(empty, empty, change, empty.size());
 	empty = empty(Rect(472, 52, 328, 778));
-	cout << "Dynamic Density using Sparse Optical Flow" << '\n';
+	cout << "Dynamic Density using Dense Optical Flow" << '\n';
 	bool next = true;
 	next = cap2.read(frame);
 	framenum++;
 	Mat temp = frame;
 	double last1 = -1, last2 = -1;
-	vector<Point2f> p0, p1;
 	cvtColor(temp, temp, COLOR_BGR2GRAY);
 	warpPerspective(temp, temp, change, temp.size());
 	temp = temp(Rect(472, 52, 328, 778));
-	Mat mask = Mat::zeros(temp.size(), temp.type());
-	fstream e("dense.csv");
-	string erf;
 	while (next)
 	{
 		bool next = cap2.read(frame);
@@ -76,28 +61,27 @@ void Sparse(string pq)
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		warpPerspective(frame, frame, change, frame.size());
 		frame = frame(Rect(472, 52, 328, 778));
-		vector<uchar> status;
-		vector<float> err;
-		goodFeaturesToTrack(temp, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
-		TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
-		calcOpticalFlowPyrLK(temp, frame, p0, p1, status, err, Size(15, 15), 2, criteria);
-		Mat dist;
-		int total = 0;
-		for (int i = 0; i < p1.size(); ++i)
-		{
-			if (norm(p1[i] - p0[i]) > 6.0)
-			{
-				total++;
-			}
-		}
+		Mat flow(temp.size(), CV_32FC2);
+		calcOpticalFlowFarneback(temp, frame, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+		Mat flow_parts[2];
+		split(flow, flow_parts);
+		Mat magnitude, angle, magn_norm;
+		cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+		normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+		angle *= ((1.0 / 360.0) *(180.0 / 255.0));
 
-		//threshold(bgr, bgr, 30, 255, THRESH_BINARY);
-		//dilate(bgr, bgr, getStructuringElement(MORPH_RECT, Size(15, 15), Point(7, 7)));
-		//  String name = "Traffic", name1 = "Queue";
-		//  namedWindow(name, WINDOW_NORMAL);
-		//  imshow(name, frame);
-		int ftotal = frame.total();
-		double pixel = (1800.0 *total) / (ftotal *1.0);
+		Mat _hsv[3], hsv, hsv8, bgr;
+		_hsv[0] = angle;
+		_hsv[1] = Mat::ones(angle.size(), CV_32F);
+		_hsv[2] = magn_norm;
+		merge(_hsv, 3, hsv);
+		hsv.convertTo(hsv8, CV_8U, 255.0);
+		cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+		cvtColor(bgr, bgr, COLOR_BGR2GRAY);
+		threshold(bgr, bgr, 30, 255, THRESH_BINARY);
+		dilate(bgr, bgr, getStructuringElement(MORPH_RECT, Size(15, 15), Point(7, 7)));
+		int total = bgr.total();
+		double pixel = (countNonZero(bgr) *1.0) / (total *1.0);
 		double pixel1 = pixel;
 		if (last2 > -1)
 		{
@@ -106,24 +90,14 @@ void Sparse(string pq)
 			pixel /= 3;
 		}
 
-		if (getline(e, erf))
-		{
-			int index = erf.find(',');
-			double val = stod(erf.substr(index + 1, erf.size() - 1 - index));
-			error1 += pow(val - pixel, 2);
-			//   cout << error1 << '\n';
-		}
-
+		//   cout << framenum << " " << pixel << "\n";
+		file << (framenum *1.0) / (15.0) << "," << pixel << endl;
 		last2 = last1;
 		last1 = pixel1;
-		temp = frame;
-		if (temp.empty())
-			break;
 	}
 
 	cap2.release();
 	destroyAllWindows();
-	error1 /= framenum;
 	return;
 }
 
@@ -133,21 +107,16 @@ int main(int argc, char **argv)
 	cor_fin.push_back(Point2f(472, 830));
 	cor_fin.push_back(Point2f(800, 830));
 	cor_fin.push_back(Point2f(800, 52));
-	file.open("extra.txt", std::ios_base::app);
+	file.open("dense.csv");
 	auto start = Clock::now();
 	if (argc == 1)
-	{
-		Sparse("vid.mp4");
-	}
+		Dense("vid.mp4");
 	else
-	{
-		Sparse(argv[1]);
-	}
-
+		Dense(argv[1]);
 	auto end = Clock::now();
 	auto dur = std::chrono::duration_cast<std::chrono::nanoseconds > (end - start).count() / pow(10, 9);
-	cout << error1 << '\n';
-	file << "Sparse Error: " << error1 << '\n';
-	file << "Sparse Runtime: " << dur << '\n';
 	file.close();
+	file1.open("extra.txt");
+	file1 << "Dense Runtime: " << dur << '\n';
+	file1.close();
 }
