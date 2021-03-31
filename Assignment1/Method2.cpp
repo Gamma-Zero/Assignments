432bf487a11da902a4169a292a60b87ce6b76244
Gamma-Zero <78369771+Gamma-Zero@users.noreply.github.com>
2021-03-31 15:18:02 +0530

    Changed. Try to generate output.

--- Assignment1/Method2.cpp
+++ Assignment1/Method2.cpp
@@ -7,9 +7,8 @@ using namespace cv;
 using namespace std;
 vector<Point2f> cor_init, cor_fin;
 double error1 = 0;
-vector<double> baseline;
 long long framenum;
-void get(int x, int y, string z)
+void get(float x, float y, string z)
 {
 	VideoCapture cap2(z);
 	if (!cap2.isOpened())
@@ -21,19 +20,6 @@ void get(int x, int y, string z)
 
 	double fps = cap2.get(CAP_PROP_FPS);
 	Mat frame, subt, img, change;
-	if (x == 328 && y == 778)
-	{
-		cout << "Method 2" << '\n';
-		fstream read("cor.csv");
-		string u;
-		while (getline(read, u))
-		{
-			int index = u.find(",");
-			cor_init.push_back(Point2f(stoi(u.substr(0, index)), stoi(u.substr(index + 1, u.size() - 1 - index))));
-		}
-
-		read.close();
-	}
 
 	cout << "Resolution: " << x << "X" << y << '\n';
 	change = findHomography(cor_init, cor_fin);
@@ -42,7 +28,7 @@ void get(int x, int y, string z)
 	cvtColor(empty, empty, COLOR_BGR2GRAY);
 	warpPerspective(empty, empty, change, empty.size());
 	empty = empty(Rect(472, 52, 328, 778));
-	resize(empty, empty, Size(x, y));
+	resize(empty, empty, Size(),x,y);
 	fstream e("stationary.csv");
 	string erf;
 	bool next = true;
@@ -54,12 +40,12 @@ void get(int x, int y, string z)
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		warpPerspective(frame, frame, change, frame.size());
 		frame = frame(Rect(472, 52, 328, 778));
-		resize(frame, frame, Size(x, y));
+		resize(frame, frame, Size(),x,y);
 		absdiff(frame, empty, subt);
 		threshold(subt, subt, 60, 255, THRESH_BINARY);
 		dilate(subt, subt, getStructuringElement(MORPH_RECT, Size(5, 5), Point(2, 2)));
 		int total = subt.total();
-		double pixel = (countNonZero(subt) *1.0) / (total *1.0);
+		double pixel = (countNonZero(subt) * 1.0) / (total * 1.0);
 		if (getline(e, erf))
 		{
 			error1 += pow((pixel - stod(erf)), 2);
@@ -73,8 +59,18 @@ void get(int x, int y, string z)
 	return;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+	cout << "Method 2" << '\n';
+	fstream read("cor.csv");
+	string u;
+	while (getline(read, u))
+	{
+		int index = u.find(",");
+		cor_init.push_back(Point2f(stoi(u.substr(0, index)), stoi(u.substr(index + 1, u.size() - 1 - index))));
+	}
+
+	read.close();
 	cor_fin.push_back(Point2f(472, 52));
 	cor_fin.push_back(Point2f(472, 830));
 	cor_fin.push_back(Point2f(800, 830));
@@ -83,12 +79,12 @@ int main(int argc, char **argv)
 	fstream file1;
 	string temp, curline, word;
 	vector<pair<int, int>> v;
-	v.push_back({ 328, 778 });
-	v.push_back({ 300, 700 });
-	v.push_back({ 300, 500 });
-	v.push_back({ 200, 700 });
-	v.push_back({ 200, 500 });
-	for (auto i: v)
+	v.push_back({ 1.0,1.0 });
+	v.push_back({ 0.8,1.0 });
+	v.push_back({ 0.8,0.8 });
+	v.push_back({ 0.8,0.6 });
+	v.push_back({ 0.5,0.5 });
+	for (auto i : v)
 	{
 		error1 = 0;
 		auto start = Clock::now();
@@ -102,9 +98,9 @@ int main(int argc, char **argv)
 		}
 
 		auto end = Clock::now();
-		auto dur = std::chrono::duration_cast<std::chrono::nanoseconds > (end - start).count() / pow(10, 9);
-		cout << sqrt(error1 / framenum *1.0) << " " << dur << setprecision(5) << '\n';
-		file << sqrt(error1 / framenum *1.0) << "," << dur << setprecision(5) << '\n';
+		auto dur = std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count() / pow(10, 9);
+		cout << sqrt(error1 / framenum * 1.0) << " " << dur << setprecision(5) << '\n';
+		file << sqrt(error1 / framenum * 1.0) << "," << dur << setprecision(5) << '\n';
 	}
 
 	file.close();
